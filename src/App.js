4cc894021d976546b68ba91ab9b857fd24964330
SongJunSub <s911130@naver.com>
2024-04-03 01:09:17 +0900

    1. React 설치 2. React Bootstrap 설치 3. navbar 구성 4. 상품 리스트 구성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
+import React, {useState} from "react";
+import simpleData from "./sampleData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [shoes] = useState(simpleData);
+
+    return (
+        <div className="App">
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Jun's Shopping Mall</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Cart</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="main-backgroundImage"></div>
+            <br/>
+
+            <Container>
+                <Row>
+                    {
+                        shoes.map((data, i) => {
+                            return (
+                                <Card shoes={shoes[i]} i={i}/>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
+        </div>
+    );
+
+}
+
+const Card = (props) => {
+
+    return (
+        <Col>
+            <img src={props.shoes.image} alt="img" width="80%" height="80%"/>
+            <h5>{props.shoes.title}</h5>
+            <p>{props.shoes.price}</p>
+        </Col>
+    )
+
 }
 
 export default App;
---
+++ src/sampleData.js
@@ -0,0 +1,25 @@
+const sampleData = [
+    {
+        id : 0,
+        title : "White and Black",
+        content : "Born in France",
+        price : 120000,
+        image : "https://image.msscdn.net/images/goods_img/20170728/595038/595038_4_500.jpg"
+    },
+    {
+        id : 1,
+        title : "Red Knit",
+        content : "Born in Seoul",
+        price : 110000,
+        image : "https://image.msscdn.net/images/goods_img/20230504/3276429/3276429_16995107122382_500.jpg"
+    },
+    {
+        id : 2,
+        title : "Grey Yordan",
+        content : "Born in the States",
+        price : 130000,
+        image : "https://image.msscdn.net/images/goods_img/20230419/3247068/3247068_16818897763243_500.jpg"
+    }
+]
+
+export default sampleData;
